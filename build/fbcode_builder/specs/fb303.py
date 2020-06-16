6f8478140c752d70103f1291e17c17085a4e42f1
Martin Thomas Fleischer <martin.fleischer@campus.tu-berlin.de>
2020-06-16 15:23:20 +0200

    [fbcode builder] Add dependency to fb303 spec
    It seems like the fbthrift python library is a dependency for the fb303
    build. Add the dependency to the fb303 spec.
    
    This assumption comes from following error message in the fb303 build:
    
    ```
    2020-06-15T17:55:15.8387750Z CMake Error at
    build/fbcode_builder/CMake/FBPythonBinary.cmake:494 (add_dependencies):
    2020-06-15T17:55:15.8388339Z   The dependency target
    "FBThrift::thrift_py.py_lib" of target
    2020-06-15T17:55:15.8388696Z   "fb303_thrift_py.py_sources_built" does
    not exist.
    2020-06-15T17:55:15.8389011Z Call Stack (most recent call first):
    2020-06-15T17:55:15.8389355Z
    build/fbcode_builder/CMake/FBThriftPyLibrary.cmake:104
    (add_fb_python_library)
    2020-06-15T17:55:15.8389697Z
    build/fbcode_builder/CMake/FBThriftLibrary.cmake:63
    (add_fbthrift_py_library)
    2020-06-15T17:55:15.8390020Z   fb303/thrift/CMakeLists.txt:15
    (add_fbthrift_library)
     ```

--- build/fbcode_builder/specs/fb303.py
+++ build/fbcode_builder/specs/fb303.py
@@ -5,13 +5,14 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import debian_specs.python_fbthrift as python_fbthrift
 import specs.fbthrift as fbthrift
 import specs.folly as folly
 
 
 def fbcode_builder_spec(builder):
     return {
-        'depends_on': [fbthrift, folly],
+        'depends_on': [fbthrift, python_fbthrift, folly],
         'steps': [
             builder.fb_github_cmake_install('fb303/_build'),
         ],
